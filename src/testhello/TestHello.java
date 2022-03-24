0fbe7309578e30c9cd23b72ded8ae50bad3128eb
Sittingy051 <peemonline000@gmail.com>
2022-03-24 13:56:05 +0700

    second commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -2,6 +2,7 @@ package testhello;
 public class TestHello {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print("Bruh World");
     }
     
 }
